0a76d196f289d15e96fbe6126525a7b94daee754
Jared <jaredwads@gmail.com>
2015-09-29 20:35:36 -0600

    Created Interfaces

---
+++ src/main/java/com/virtus/JLL/SemiSupervised/ITest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.virtus.JLL.SemiSupervised;
+
+import com.virtus.Data.Set;
+
+/**
+ *
+ * @author jared
+ */
+public interface ITest {
+    public ITestResult runTest(Set testData);
+    
+    
+}
