b88ec28e312aba4b42751f872b05c84febe4e0dc
Jared <jaredwads@gmail.com>
2015-10-01 10:44:59 -0600

    implemented array initialization

--- src/main/java/com/virtus/JUtils/Arrays/ArrayInitializer.java
+++ src/main/java/com/virtus/JUtils/Arrays/ArrayInitializer.java
@@ -5,6 +5,9 @@
  */
 package com.virtus.JUtils.Arrays;
 
+import com.virtus.JMath.Shuffler;
+import com.virtus.JUtils.Rand;
+
 /**
  *
  * @author jared
@@ -14,14 +17,34 @@ public final class ArrayInitializer {
         int array[] = new int[pSize];
         
         for(int i = 0; i < pSize; i++) {
-            
+            array[i] = Rand.Int(pMax, pMin);
         }
-        
-        
-        return null;
+        return array;
     }
     
-    public static double randomDouble() {
+    public static double[] randomDouble(int pSize, double pMax, double pMin) {
+        double array[] = new double[pSize];
         
+        for(int i = 0; i < pSize; i++) {
+            array[i] = Rand.Double(pMax, pMin);
+        }
+        return array;
+    }
+    
+    public static int[] sequential(int pSize) { return sequential(pSize, 0); }
+    public static int[] sequential(int pSize, int pStart) {
+        int array[] = new int[pSize];
+        
+        for(int i = 0; i < pSize; i++) {
+            array[i] = i + pStart;
+        }
+        return array;
+    }
+    
+    public static int[] sequentialShuffled(int pSize) {
+        return sequentialShuffled(pSize, 0);
+    }
+    public static int[] sequentialShuffled(int pSize, int pStart) {
+        return Shuffler.FisherYates(sequential(pSize, pStart));
     }
 }
