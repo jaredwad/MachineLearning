34064f514f8f37cd384d8f41fb2198db3b5d8f08
Jared <jaredwads@gmail.com>
2015-10-01 10:44:31 -0600

    Removed duplicate files

--- src/main/java/com/virtus/JLL/SemiSupervised/ITest.java
+++ src/main/java/com/virtus/JLL/SemiSupervised/ITest.java
@@ -5,15 +5,32 @@
  */
 package com.virtus.JLL.SemiSupervised;
 
+import com.virtus.Data.Row;
 import com.virtus.Data.Set;
 
 /**
  *
  * @author jared
  */
-public interface ITest {
-    public ITestResult runTest(Set testData);
 public abstract class ITest implements Runnable {
+    protected Set training;
+    protected Set test;
     
+    protected ITest(double pPercentTest, Set pData) {
+        splitData(pPercentTest, pData);
+    }
     
+    protected void splitData(double pPercentTest, Set pData) {
+        training = new Set();
+        test     = new Set();
+        
+        int numRows = pData.NumRows();
+        
+        for(int i = 0; i < numRows; ++i) {
+            Row row = pData.getRow(i);
+            
+            
+            
+        }
+    }
 }
