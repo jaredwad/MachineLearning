c7e7b0154a73374ae00c145f8b9ba0006f8f0efc
Jared <jaredwads@gmail.com>
2015-10-12 12:01:14 -0600

    Added functionality to run

--- src/main/java/com/virtus/JLL/Supervised/KNN.java
+++ src/main/java/com/virtus/JLL/Supervised/KNN.java
@@ -9,13 +9,12 @@ import com.virtus.Data.IColumn;
 import com.virtus.Data.Row;
 import com.virtus.Data.Set;
 import com.virtus.Data.Types.DataItem;
+import com.virtus.Data.Types.Discrete;
 import com.virtus.JLL.SemiSupervised.ITest;
 import com.virtus.exception.InvalidTYPEException;
 import javafx.util.Pair;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  *
@@ -24,23 +23,44 @@ import java.util.List;
 public class KNN extends ITest {
 
     private int numNeighbors;
+	private int numCorrect;
+	private int numFalse;
 
-    KNN (Set pData) { this(pData, 3, .7); }
+
+	KNN (Set pData) { this(pData, 3, .7); }
     KNN (Set pData, int pNumNeighbors) { this(pData, pNumNeighbors, .7); }
     KNN (Set pData, int pNumNeighbors, double pPercentTest) {
         super(pPercentTest, pData);
         numNeighbors = pNumNeighbors;
+		numCorrect = 0;
+		numFalse = 0;
     }
     
     @Override
     public void run() {
-        
+		List<Row> rows = test.getRows();
+		int len = rows.size();
+
+
+        for(int i = 0; i < len; ++i) {
+			try {
+				DataItem pred = mostCommon(getNeighbors(rows.get(i)));
+				DataItem act  = test.getClassification(i);
+
+				if(pred.equals(act)) { numCorrect++; }
+				else                 { numFalse  ++; }
+			} catch (InvalidTYPEException e) {
+				e.printStackTrace();
+			}
+		}
     }
+
+	public int getCorrect() { return numCorrect; }
+	public int getFalse()   { return numFalse;   }
 
     public List<DataItem> getNeighbors(Row row) throws InvalidTYPEException {
         IColumn classifications = training.getClassCol();
         int numRows = training.NumRows();
-
 
         if(numNeighbors > numRows)
             return classifications.getItems();
@@ -53,6 +73,11 @@ public class KNN extends ITest {
 
         Collections.sort(neighbors);
 
+        //TODO erase this
+        for(int i = 0; i < numRows; ++i) {
+            System.out.println(neighbors.get(i));
+        }
+
         ArrayList<DataItem> kNeighbors = new ArrayList<>(numNeighbors);
 
         for(int i = 0; i < numNeighbors; ++i) {
@@ -61,6 +86,30 @@ public class KNN extends ITest {
 
         return kNeighbors;
     }
+
+	public static DataItem mostCommon(List<DataItem> list) {
+		Map<DataItem, Integer> map = new HashMap<>();
+
+		System.out.println("The list contains the values: ");
+
+		for (DataItem t : list) {
+			System.out.println("\t" + ((Discrete)t).getValue());
+
+			Integer val = map.get(t);
+			map.put(t, val == null ? 1 : val + 1);
+		}
+
+		Map.Entry<DataItem, Integer> max = null;
+
+		for (Map.Entry<DataItem, Integer> e : map.entrySet()) {
+			if (max == null || e.getValue() > max.getValue())
+				max = e;
+		}
+
+		System.out.print  ("The most common value was: ");
+		System.out.println(((Discrete)max.getKey()).getValue() + "\n");
+		return max.getKey();
+	}
 
     public DataItem getClass(List<DataItem> pItems) {
         return null;
