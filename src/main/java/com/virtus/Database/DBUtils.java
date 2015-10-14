02b4639827e7d08a706efb645ca465cccabf3ea8
Jared <jaredwads@gmail.com>
2015-10-13 22:04:46 -0600

    Added get columns from type

--- src/main/java/com/virtus/Database/DBUtils.java
+++ src/main/java/com/virtus/Database/DBUtils.java
@@ -1,14 +1,13 @@
 package com.virtus.Database;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
-import com.virtus.Data.IColumn;
-import com.virtus.Data.TYPE;
+import com.virtus.Data.*;
+import com.virtus.Data.Types.Binary;
+import com.virtus.Data.Types.Discrete;
+import com.virtus.Data.Types.Numeric;
+import com.virtus.exception.InvalidTYPEException;
 import org.h2.tools.DeleteDbFiles;
 
 public class DBUtils {
@@ -58,12 +57,6 @@ public class DBUtils {
     			+ " petallength, petalwidth, class)"
     			+ " SELECT * FROM CSVREAD('" + filePath + "')";
     	
-    	/*
-    	String toData =  "INSERT INTO DATA (sepallength, sepalwidth,"
-    			+ " petallength, petalwidth, class) VALUES"
-    			+ " (1,2,3,4,'test');";
-    	*/
-    	
     	String toClass = "INSERT INTO CLASSIFICATION SELECT id, class FROM DATA";
     	
     	String alter = "UPDATE DATA SET class = NULL";
@@ -74,8 +67,7 @@ public class DBUtils {
             
             stmt.execute(toData);
             stmt.execute(toClass);
-            stmt.execute(alter);
-            
+//            stmt.execute(alter);
             
         } catch (Exception ex) {
         	ex.printStackTrace();
@@ -87,25 +79,32 @@ public class DBUtils {
 
 	public static IColumn getColumnFromDB(String colName, TYPE type) throws SQLException {
 		System.out.println("Getting column " + colName + " from the Database");
-		IColumn col = null;
+		IColumn col = TYPE.getColumnFromTYPE(type);
 
 		Connection connection = getDBConnection();
-		Statement stmt = null;
+		Statement stmt;
 
-		String filePath = new File("lib/Iris.csv").getAbsolutePath();
-
-		String toData =  "INSERT INTO DATA (sepallength, sepalwidth,"
-				+ " petallength, petalwidth, class)"
-				+ " SELECT * FROM CSVREAD('" + filePath + "')";
+		String query =  "SELECT " + colName + " FROM DATA";
 
 		try {
 			connection.setAutoCommit(false);
 			stmt = connection.createStatement();
 
-			stmt.execute(toData);
+			ResultSet rs = stmt.executeQuery(query);
+
+			switch(type) {
+				case Binary:
+					col = getBinaryCol(rs);
+					break;
+				case Numeric:
+					col = getNumericCol(rs);
+					break;
+				default:
+					col = getDiscreteCol(rs);
+			}
 
 
-		} catch (Exception ex) {
+        } catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
 			connection.commit();
@@ -114,12 +113,70 @@ public class DBUtils {
 
 		return col;
 	}
+
+	public static IColumn getBinaryCol(ResultSet rs) throws SQLException, InvalidTYPEException {
+		BinaryColumn col = new BinaryColumn();
+
+		while (rs.next()) {
+			Binary b = new Binary(rs.getBoolean(1));
+
+			col.add(b);
+		}
+
+		return col;
+	}
+
+	public static IColumn getNumericCol(ResultSet rs) throws SQLException, InvalidTYPEException {
+		NumericColumn col = new NumericColumn();
+
+		while (rs.next()) {
+			Numeric n = new Numeric(rs.getDouble(1));
+
+			col.add(n);
+		}
+
+		return col;
+	}
+
+	public static IColumn getDiscreteCol(ResultSet rs) throws SQLException, InvalidTYPEException {
+		DiscreteColumn col = new DiscreteColumn();
+
+		while (rs.next()) {
+			Discrete d = new Discrete(rs.getString(1));
+
+			col.add(d);
+		}
+
+		return col;
+	}
+
+	public static TYPE getType(int type) {
+		switch (type) {
+			case Types.BINARY:
+			case Types.BIT:
+			case Types.BOOLEAN:
+				return TYPE.Binary;
+
+			case Types.BIGINT:
+			case Types.DECIMAL:
+			case Types.DOUBLE:
+			case Types.FLOAT:
+			case Types.INTEGER:
+			case Types.SMALLINT:
+			case Types.TINYINT:
+				return TYPE.Numeric;
+
+			//Any of the *char, Date, etc.
+			default:
+				return TYPE.Discrete;
+		}
+	}
     
     public static void DisplayTables() throws SQLException {
     	System.out.println("Starting Display Tables");
     	
     	Connection connection = getDBConnection();
-        Statement stmt = null;
+        Statement stmt;
         
         try {
         	connection.setAutoCommit(false);
